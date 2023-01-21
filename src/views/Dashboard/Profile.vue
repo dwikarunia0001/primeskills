4e570fd90abead195076867fad9eb1a6e812037d
dwikarunia0001 <dwikarunia14157375@gmail.com>
2023-01-21 21:37:31 +0700

    setup vercel

--- src/views/Dashboard/Profile.vue
+++ src/views/Dashboard/Profile.vue
@@ -17,7 +17,7 @@
             <div class="col-auto">
               <div class="avatar avatar-xl position-relative">
                 <img
-                  src="../assets/img/team-1.jpg"
+                  src="../../assets/img/team-1.jpg"
                   alt="profile_image"
                   class="shadow-sm w-100 border-radius-lg"
                 />
