cfe2c6f9b29ab66f29e361e5a2e2d4f1397df667
dwikarunia0001 <dwikarunia14157375@gmail.com>
2023-01-23 01:19:27 +0700

    responsive welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="text-white" style="height:590px">
     <main class="position-absolute top-50 start-50 translate-middle ">
+        
         <div class="container">
-            <div class="row">
-                <div class="col">
-                Column
-                </div>
-                <div class="col">
-                Column
-                </div>
-                <div class="col">
-                Column
-                </div>
+            <div class="row justify-content-center">
+            <img src="../assets/img/minerva-sorting-hat.webp" alt="minerva-sorting-hat" width="160" height="180" class="col-lg-4 col-sm-7 mx-auto d-block rounded-circle">
+            <p class="col-lg-10 col-sm-8 text-start mt-4">"Welcome to Hogwarts. </p>
+            <p class="col-lg-10 col-sm-8 text-start ">In a moment, you will pass through these doors...but before you can take your seats, you must be sorted into your houses. They are #Gryffindor, #Hufflepuff, #Ravenclaw, and #Slytherin. While here, your house is, in many ways, your family."</p>
             </div>
-        </div>
+        </div>   
+
     </main>
   </div>
 </template>
